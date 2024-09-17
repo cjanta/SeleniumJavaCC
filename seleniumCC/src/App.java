ecea12a77d5c4a524136cb49febe5fc24c9cb66c
Sven Kanter <svkanter@gmail.com>
2024-09-17 10:00:13 +0200

    bereit eingeloggt prüfung; refactoring

--- seleniumCC/src/App.java
+++ seleniumCC/src/App.java
@@ -6,12 +6,56 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import java.time.Duration;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) throws Exception {
         WebDriver driver = getWebDriver(args);
-        
         driver.get("https://portal.cc-student.com/index.php?cmd=login");
+        boolean isUserLoggedIn = isUserAlreadyLoggedIn(driver);
+
+        if(!isUserLoggedIn){
+            loginToWebsiteWithCredentials(args, driver);
+            isUserLoggedIn = true;
+        }
+
+        if(isUserLoggedIn){
+            navigateToTimeRecordingMenu(driver);
+            clickOpenTimeRecordingButton(driver);
+            clickFinalTimeRecordingButton(driver);
+        }
+ 
+    }
+
+    private static void clickFinalTimeRecordingButton(WebDriver driver){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        By finalButtonPath = By.name("kommengehenbutton");
+        WebElement finalButton = wait.until(ExpectedConditions.elementToBeClickable((finalButtonPath)));
+        finalButton.click();
+    }
+
+    private static void navigateToTimeRecordingMenu(WebDriver driver){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        By timeRecordingMenu = By.xpath("//a[contains(@href, 'cmd=kug')]");
+        WebElement timeRecordingMenuButton = wait.until(ExpectedConditions.elementToBeClickable(timeRecordingMenu));
+        timeRecordingMenuButton.click();
+        
+    }
+
+    private static void clickOpenTimeRecordingButton(WebDriver driver){
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
+        By zeiterfassung_oeffnen_button_path = By.xpath("//button[contains(@name, 'showDialogButton')]");
+        WebElement zeiterfassung_oeffnen_button = wait.until(ExpectedConditions.elementToBeClickable((zeiterfassung_oeffnen_button_path)));
+        zeiterfassung_oeffnen_button.click();
+    }
+
+    private static boolean isUserAlreadyLoggedIn(WebDriver driver){
+        By logout = By.xpath("//a[contains(@href, 'cmd=logout')]");
+        List<WebElement> results = driver.findElements(logout);
+        return !results.isEmpty();
+    }
+
+    private static void loginToWebsiteWithCredentials(String[] args, WebDriver driver){
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(20));
 
         By loginUsername = By.name("login_username");
@@ -26,18 +70,6 @@ public class App {
         By loginButtonPath = By.xpath("//input[@type='submit']");
         WebElement login_button = driver.findElement(loginButtonPath);
         login_button.click();
-
-        By timeRecordingButtonPath = By.xpath("//a[contains(@href, 'cmd=kug')]");
-        WebElement zeiterfassung_button = wait.until(ExpectedConditions.elementToBeClickable(timeRecordingButtonPath));
-        zeiterfassung_button.click();
-
-        By zeiterfassung_oeffnen_button_path = By.xpath("//button[contains(@name, 'showDialogButton')]");
-        WebElement zeiterfassung_oeffnen_button = wait.until(ExpectedConditions.elementToBeClickable((zeiterfassung_oeffnen_button_path)));
-        zeiterfassung_oeffnen_button.click();
-
-        By finalButtonPath = By.name("kommengehenbutton");
-        WebElement finalButton = wait.until(ExpectedConditions.elementToBeClickable((finalButtonPath)));
-        finalButton.click();
     }
 
     private static String getUsernameFromArgs(String[] args){
